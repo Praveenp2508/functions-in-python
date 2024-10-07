892afb637a740e48ad422db8777ba687b36c62b6
Praveen P <107240891+Praveenp2508@users.noreply.github.com>
2024-10-07 18:57:44 +0530

    Add files via upload

---
+++ class11.py
@@ -0,0 +1,16 @@
+#funtion
+
+# def add():
+#     b=int(input('enter number'))
+#     c=int(input('enter number'))
+#     a=b+c
+#     print(a)
+# add()
+
+# def even(a):
+#         if a % 2 == 0:
+#             print('even number')
+#         else:
+#             print('odd number')
+# even(10)
+
---
+++ class12.py
@@ -0,0 +1,23 @@
+# name='praveen'
+# age=22
+# salary='10,000'
+#
+# print(f"my name is {name} age is {age} salary is {salary}")
+
+#multipule argument
+
+# def addsum(*k):
+#     r=0
+#     for i in k:
+#         r=r+i
+#     return r
+# print(addsum(20,20,20))
+
+#argument key values pair
+
+# def addsum(**k):
+#     r=''
+#     for i in k.values():
+#         r=r+i
+#     return r
+# print(addsum(a='python', b=' is good'))
